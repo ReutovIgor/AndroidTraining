b47027fa5df76383974b3012454ae4579f368fc4
Igor Reutov <vextrell@gmail.com>
2018-06-21 11:20:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ruireutov/androidtrainingproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ruireutov.androidtrainingproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
